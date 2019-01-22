1adf2388a0532c438f404ed5d4ce2eca004e0638
Oksana Bibik <Oksanatishka@users.noreply.github.com>
2019-01-21 23:24:25 -0800

    Update Jaime.class.php

--- D07 - MOAR OBJECT ORIENTED PROGRAMMING/ex04/Jaime.class.php
+++ D07 - MOAR OBJECT ORIENTED PROGRAMMING/ex04/Jaime.class.php
@@ -5,6 +5,7 @@
 			if ($person instanceof Cersei) {
 				print("With pleasure, but only in a tower in Winterfell, then." . PHP_EOL);
 			}
+			// is_subclass_of — Checks if the object has this class as one of its parents or implements it
 			else if (is_subclass_of($person, Lannister)) {
 				print("Not even if I'm drunk !" . PHP_EOL);
 			}
