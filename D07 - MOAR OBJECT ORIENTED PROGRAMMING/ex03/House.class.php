723f28321931070894eab5ea25e5633ea7f6c4a0
Oksana Bibik <Oksanatishka@users.noreply.github.com>
2019-01-21 23:17:13 -0800

    Update House.class.php

--- D07 - MOAR OBJECT ORIENTED PROGRAMMING/ex03/House.class.php
+++ D07 - MOAR OBJECT ORIENTED PROGRAMMING/ex03/House.class.php
@@ -4,6 +4,7 @@
 		public function introduce() {
 			// print — Output a string
 			// printf — Output a formatted string
+			// print('House ' . $this->getHouseName() . ' of ' . $this->getHouseSeat() . ' : ' . '"' . $this->getHouseMotto() . '"' . PHP_EOL);
 			printf("House %s of %s : \"%s\"\n", $this->getHouseName(), $this->getHouseSeat(), $this->getHouseMotto());
 		}
 	}
