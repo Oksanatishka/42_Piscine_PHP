26f05b76ee628a7d224250fba1965290756c95b1
Oksana Bibik <Oksanatishka@users.noreply.github.com>
2019-01-21 23:14:24 -0800

    Update House.class.php

--- D07 - MOAR OBJECT ORIENTED PROGRAMMING/ex03/House.class.php
+++ D07 - MOAR OBJECT ORIENTED PROGRAMMING/ex03/House.class.php
@@ -2,6 +2,8 @@
 	class House
 	{
 		public function introduce() {
+			// print — Output a string
+			// printf — Output a formatted string
 			printf("House %s of %s : \"%s\"\n", $this->getHouseName(), $this->getHouseSeat(), $this->getHouseMotto());
 		}
 	}
